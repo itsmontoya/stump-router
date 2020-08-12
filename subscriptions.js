6270edc8b3cd733d4fa8fdaddbe11b75fa0c4393
Josh Montoya <josh@montoya.io>
2020-08-12 14:11:30 -0700

    remove console log

--- subscriptions.js
+++ subscriptions.js
@@ -2,8 +2,5 @@ import { dispatcher } from "stump";
 import { setCurrentRoute } from "./router.js";
 
 export const onURLChange = dispatcher(dispatch => {
-	addEventListener("popstate", _ => {
-		console.log("POPSTATE");
-		dispatch(setCurrentRoute);
-	})
+	addEventListener("popstate", _ => dispatch(setCurrentRoute));
 });
