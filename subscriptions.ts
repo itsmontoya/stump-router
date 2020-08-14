48efa8530915bfcaa1046daeecb86aca1155f476
Josh Montoya <josh@montoya.io>
2020-08-14 10:11:24 -0700

    remove subscriptions

--- subscriptions.ts
+++
@@ -1,6 +0,0 @@
-import { dispatcher } from "stump";
-import { setCurrentRoute } from "./router";
-
-export const onURLChange = dispatcher(dispatch => {
-	addEventListener("popstate", _ => dispatch(setCurrentRoute));
-});
